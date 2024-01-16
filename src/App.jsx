ec521c67e746fe67d7c7258964be0e5ef4656f34
Sofia Genchi <sofigen16@gmail.com>
2024-01-16 13:26:20 -0300

    building the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    nav
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      hero
+    </section>
+    <section className="padding">
+      popularproductos
+    </section>
+    <section className="padding">
+      superquality
+    </section>
+    <section className="padding-x py-10">
+      services
+    </section>
+    <section className="padding">
+      specialoffers
+    </section>
+    <section className="bg-pale-blue padding">
+      customersreviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      subscirbes
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      footer
+    </section>
+  </main>
+);
+
+export default App;
