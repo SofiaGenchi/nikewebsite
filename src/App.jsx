7e1e73bd801a6fb1d77eac0eeffc03da5d83ba15
Sofia Genchi <sofigen16@gmail.com>
2024-01-16 14:29:20 -0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import Nav from './components/Nav';
 import { CustomerReviews, Footer, Hero, PopularProducts, Services, SpecialOffer, Subscribe, SuperQuality } from './sections'
 
 const App = () => (
   <main className="relative">
-    nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
       <Hero />
     </section>
